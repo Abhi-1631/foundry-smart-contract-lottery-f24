4f03255ac2ae9538f284073451675c5b073bda89
Abhi1631 <abhijeetkumarkeshri@gmail.com>
2024-04-05 12:18:27 +0530

    added new tests

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.8.19;
 
 contract HelperConfig is Script {
     struct NetworkConfig {
-        uint256 enteranceFee;
+        uint256 entranceFee;
         uint256 interval;
         address vrfCoordinator;
         bytes32 gasLane;
@@ -18,17 +18,17 @@ contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
 
     constructor() {
-        if (block.chainid = 11155111) {
-            activeNetworkConfig == getSeopliaEthConfig();
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSeopliaEthConfig();
         } else {
-            activeNetworkConfig == getOrCreateAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
     function getSeopliaEthConfig() public pure returns (NetworkConfig memory) {
         return
             NetworkConfig({
-                enteranceFee: 0.01 ether,
+                entranceFee: 0.01 ether,
                 interval: 30,
                 vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
@@ -39,7 +39,7 @@ contract HelperConfig is Script {
 
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
         if (activeNetworkConfig.vrfCoordinator != address(0)) {
-            return NetworkConfig;
+            return activeNetworkConfig;
         }
 
         uint96 baseFee = 0.25 ether; // 0.25 LINK
@@ -56,7 +56,7 @@ contract HelperConfig is Script {
 
         return
             NetworkConfig({
-                enteranceFee: 0.01 ether,
+                entranceFee: 0.01 ether,
                 interval: 30,
                 vrfCoordinator: address(vrfCoordinatorMock),
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
